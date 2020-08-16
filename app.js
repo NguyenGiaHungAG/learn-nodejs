20492f77bca23faeeaa33bc37ae004f63b807068
NguyenGiaHung <nguyengiahungangiang@gmail.com>
2020-08-16 22:46:31 +0700

    Install Express module and create server

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Nodejs");
+const express = require('express')
+const { response } = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (request, response) => {
+    response.send('Hello Hung')
+})
+
+app.listen(port, () => {
+    console.log(`Server listening at http://localhost: ${port}`)
+})
