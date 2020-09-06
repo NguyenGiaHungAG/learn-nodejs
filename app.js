52f0b1bfba1b7e507c590de195f5dcef4ce5ce57
NguyenGiaHung <nguyengiahungangiang@gmail.com>
2020-09-06 22:21:15 +0700

    create post function

--- app.js
+++ app.js
@@ -3,9 +3,38 @@ const { response } = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (request, response) => {
-    response.send('Hello Hung')
-})
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
+
+app.set('view engine', 'pug')
+app.set('views', './views')
+
+users = [
+  {Name: "Hung", Age: 23},
+  {Name: "Thinh", Age: 25}
+];
+app.get('/', function(req, res){
+  res.render('index');
+});
+app.get('/users', function (req, res) {
+    res.render('users/index', {users: users});
+  });
+app.get('/users/search', function(req, res){
+  var q = req.query.q;
+  var matchedUsers = users.filter(user => {
+    return user.Name.toLowerCase().indexOf(q.toLowerCase()) !== -1
+  });
+  res.render('users/index', {users: matchedUsers, q:q});
+});
+
+app.get('/users/create', function(req, res){
+  res.render('users/create');
+});
+
+app.post('/users/create', function(req, res){
+  users.push(req.body);
+  res.render('users/index', {users: users});
+});
 
 app.listen(port, () => {
     console.log(`Server listening at http://localhost: ${port}`)
